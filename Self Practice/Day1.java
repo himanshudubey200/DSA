b6e79d2c87a97799975ad856151010204c50e288
himanshudubey200 <himanshudubeyi.a.s2006@gmail.com>
2026-01-14 22:51:21 +0530

    second push

--- Self Practice/Day1.java
+++ Self Practice/Day1.java
@@ -1,41 +1,14 @@
 public class Day1 {
-
     public static void merge(int[] nums1, int m, int[] nums2, int n) {
 
-        int[] temp = new int[m + n];
+        int i = m - 1, j = n - 1, k = m + n - 1;
 
-        int i = 0, j = 0, idx = 0;
-
-        while (i < m && j < n) {
-            if (nums1[i] <= nums2[j]) {
-                temp[idx++] = nums1[i++];
+        while (j >= 0) {
+            if (i >= 0 && nums1[i] > nums2[j]) {
+                nums1[k--] = nums1[i--];
             } else {
-                temp[idx++] = nums2[j++];
+                nums1[k--] = nums2[j--];
             }
-        }
-
-        while (i < m) {
-            temp[idx++] = nums1[i++];
-        }
-
-        while (j < n) {
-            temp[idx++] = nums2[j++];
-        }
-
-        for (int x = 0; x < m + n; x++) {
-            nums1[x] = temp[x];
-        }
-    }
-
-    public static void main(String[] args) {
-        int[] nums1 = {1, 2, 3, 0, 0, 0};
-        int[] nums2 = {2, 5, 6};
-        int m = 3, n = 3;
-
-        merge(nums1, m, nums2, n);
-
-        for (int num : nums1) {
-            System.out.print(num + " ");
         }
     }
 }
